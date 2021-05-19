f80d297a281b4d9eff31f89f00757333d6c43d10
khmr-2622 <intelligentharsh007@gmail.com>
2021-05-19 13:56:28 +0530

    some changes

--- api/app.py
+++ api/app.py
@@ -659,4 +659,4 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0',threaded=True)
